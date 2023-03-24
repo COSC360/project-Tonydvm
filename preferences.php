54d2047dd88bedf9d43967a4214759d518b2f120
Antonio Vazquez-Mackay <73569624+chunkychund@users.noreply.github.com>
2023-03-24 14:03:49 -0700

    bug

--- preferences.php
+++ preferences.php
@@ -34,7 +34,7 @@ try{
     $userId = $_SESSION['user'];
 
     // get user info from database
-    $sql = "SELECT * FROM users WHERE userId = :userId";
+    $sql = "SELECT * FROM users WHERE userId = :id";
     $statement = $pdo->prepare($sql);
     $statement->execute(['userId' => $userId]);
 
