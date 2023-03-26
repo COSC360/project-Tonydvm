a3eea87f9c647316fa1fe0a4fc397436e405a6db
Antonio Vazquez-Mackay <73569624+chunkychund@users.noreply.github.com>
2023-03-26 16:59:45 -0700

    admin page

---
+++ add_price.php
@@ -0,0 +1,36 @@
+<?php
+try {
+    $connString = "mysql:host=localhost;dbname=db_76865732";
+    $user = "76865732";
+    $pass = "76865732";
+
+    $pdo = new PDO($connString, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    //    get post data (product s, store s, price decimal 10,2, date mysql date)
+    $product = $_POST['product'];
+    $store = $_POST['store'];
+    $price = $_POST['price'];
+    $date = $_POST['date'];
+
+    //    sanitize data
+    $price = trim($price);
+    $date = trim($date);
+
+    //   add price to grocery_item_prices table (id, grocery_item_id, store_id, price, price_date)
+    $sql = "INSERT INTO grocery_item_prices (grocery_item_id, store_id, price, price_date) VALUES (:product, :store, :price, :date)";
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(':product', $product);
+    $statement->bindValue(':store', $store);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':date', $date);
+    $statement->execute();
+
+    //    redirect to preferences.php with success message
+    header("Location: preferences.php?success=1");
+
+
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
--- preferences.php
+++ preferences.php
@@ -38,6 +38,12 @@
       $statement = $pdo->prepare($sql);
       $statement->execute();
 
+      // check if role is admin
+      $sql3 = "SELECT role FROM users WHERE id = $user_id";
+      $statement3 = $pdo->prepare($sql3);
+      $statement3->execute();
+      $role = $statement3->fetchColumn();
+
       // get image from database user_images table (id, user_id, image_url(varchar))
       $sql2 = "SELECT image_url FROM user_images WHERE user_id = $user_id";
       $statement2 = $pdo->prepare($sql2);
@@ -58,11 +64,59 @@
           </div>
           ';
       }
+      // if role is admin, include options to add price to grocery_item_prices table (id, grocery_item_id, store_id, price, price_date) from list of products from grocery_items table (id, name, brand, category_name, description, image_url, weight)
+      if ($role == 'admin') {
+        // query for list of products with id and name
+        $sql4 = "SELECT id, name FROM grocery_items";
+        $statement4 = $pdo->prepare($sql4);
+        $statement4->execute();
+
+        // query for list of stores with id and name
+        $sql5 = "SELECT id, name FROM stores";
+        $statement5 = $pdo->prepare($sql5);
+        $statement5->execute();
+
+        echo '
+          <div class="admin-options">
+            <h2>Admin Options</h2>
+            <form action="add_price.php" method="post">
+              <label for="product">Product:</label>
+              <select name="product" id="product">
+                <option value="">Select Product</option>
+                ';
+        while ($row = $statement4->fetch()) {
+          echo '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
+        }
+        echo '
+              </select>
+              <label for="store">Store:</label>
+              <select name="store" id="store">
+                <option value="">Select Store</option>
+                ';
+        while ($row = $statement5->fetch()) {
+          echo '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
+        }
+        echo '
+              </select>
+              <label for="price">Price:</label>
+              <input type="text" id="price" name="price" placeholder="Price" />
+              <label for="date">Date:</label>
+              <input type="text" id="date" name="date" placeholder="Date (YYYY-MM-DD)" />
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+          ';
+      }
+
+      // check get variable for success message
+      if (isset($_GET['success'])) {
+        echo '<h2>' . $_GET['success adding price'] . '</h2>';
+      }
+
     } catch (PDOException $e) {
       die($e->getMessage());
     }
     ?>
-
     <!-- log out button ends php session -->
     <a href="logout.php">
       <div class="button" style="width:20em;">
