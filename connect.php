cacbb446171b8315378ef8e55b497d6d62c0059d
Antonio Vazquez-Mackay <73569624+chunkychund@users.noreply.github.com>
2023-04-03 15:39:12 -0700

    db test

--- connect.php
+++ connect.php
@@ -1,6 +1,9 @@
 <?php
     try {
-        require_once 'connectDetails.php';
+        // require_once($_SERVER['DOCUMENT_ROOT']."/connectDetails.php");
+        require_once($_SERVER['DOCUMENT_ROOT']."/project-Tonydvm/connectDetails.php");
+
+        echo $connString;
 
         $pdo = new PDO($connString, $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
